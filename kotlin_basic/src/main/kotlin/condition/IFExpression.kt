6d6f056e7b4e8b4a4511492ce14d68f88ca4ca23
bellukchips <bellukchips@gmail.com>
2023-08-01 20:51:20 +0700

    learn(ifExpression) : if expression

---
+++ kotlin_basic/src/main/kotlin/condition/IFExpression.kt
@@ -0,0 +1,12 @@
+package condition
+
+fun main() {
+    val nilai = 70
+    if(nilai > 80){
+        println("Nice...")
+    }else if(nilai > 75){
+        println("Nice Try")
+    }else{
+        println("Sorry")
+    }
+}

9380d3249b829fe056690e073785ec3a9066387c
bellukchips <bellukchips@gmail.com>
2023-08-01 12:54:06 +0700

    learn(TypeDataCharacter) : type data character

---
+++ kotlin_basic/src/main/kotlin/type_data/TypeDataCharacter.kt
@@ -0,0 +1,11 @@
+package type_data
+
+fun main() {
+    // only 1 character for char
+    var s: Char = 'S'
+    var l: Char = 'L'
+    var w: Char = 'W'
+    print(s)
+    print(l)
+    print(w)
+}
